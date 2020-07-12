554d94b01eac97a3a29d4616e33ea6654a071771
aljrico <aljrico@gmail.com>
2020-07-12 19:11:52 +0100

    one metric map

--- R/mod_cloropleth.R
+++ R/mod_cloropleth.R
@@ -19,63 +19,29 @@ mod_cloropleth_ui <- function(id) {
 #' @noRd
 mod_cloropleth_server <- function(input, output, session, rv, global) {
   ns <- session$ns
-  
+
   observeEvent(input$cloropleth_shape_click, {
     rv$selected_country <- input$cloropleth_shape_click$id
   })
-  
 
   output$cloropleth <-
     leaflet::renderLeaflet({
-      create_gradient <- function(col1, col2) {
-        fn_cols <- grDevices::colorRamp(c(col1, col2), space = "Lab", interpolate = "spline")
-        cols <- fn_cols(seq(0, 1, length.out = 10)) / 255
-        grDevices::rgb(cols[, 1], cols[, 2], cols[, 3], alpha = 1)
-      }
-      
-      get_quantiles <- function(metric, n = 666){
-        qs <- seq(from = 0, to = 1, by = 1/ n)
-        bins <- unique(floor(quantile(log(metric), qs) %>% as.vector()))
-        c(bins, Inf)
-      }
-
-      if (rv$selected_variable == "infected") {
-        title <- "Confirmed Cases"
-        column <- "confirmed_cases"
-        colours <- create_gradient(col1 = global$colours$grey, col2 = global$colours$orange)
-        my_bins <- c(0, 1, 1e2, 1e3, 1e4, 1e5, Inf)
-      } else if (rv$selected_variable == "deaths") {
-        title <- "Deaths"
-        column <- "confirmed_deaths"
-        colours <- create_gradient(col1 = global$colours$grey, global$colours$red)
-        my_bins <- c(0, 1, 1e2, 1e3, 1e4, 2e4, Inf)
-      } else if (rv$selected_variable == "recovered") {
-        title <- "Recovered"
-        column <- "confirmed_recovered"
-        colours <- create_gradient(col1 = global$colours$grey, global$colours$blue)
-        my_bins <- c(0, 1e2, 1e3, 1e4, 1e5, Inf)
-      }
-
-      # Prepare text for the tooltip
-      mytext <- paste0(
-        "<b> Country: </b> ", rv$map_data$NAME, "<br/>",
-        "<b> ", title, ": </b> ", prettyNum(rv$map_data[[column]], big.mark = ","), "<br/>"
-      ) %>%
-        lapply(htmltools::HTML)
-
-      my_palette <- leaflet::colorBin(colours, rv$map_data[[column]], na.color = "white", bins = get_quantiles(rv$map_data[[column]]))
 
       # Define options
-      leaflet_options <- leaflet::leafletOptions(
-        minZoom = 2,
-        controlZoom = FALSE
-      )
+      leaflet_options <- function() {
+        leaflet::leafletOptions(
+          minZoom = 2,
+          controlZoom = FALSE
+        )
+      }
+
+      # Design legend
+      legend_hint <- function() {
+          tags$p("Click on a specific country for more details.")
+      }
 
       # Build Cloropleth
-      leaflet::leaflet(rv$map_data, options = leaflet::leafletOptions(
-        minZoom = 2,
-        zoomControl = FALSE
-      )) %>%
+      leaflet::leaflet(rv$map_data, options = leaflet_options()) %>%
         leaflet::setMaxBounds(
           lng1 = -120,
           lat1 = -80,
@@ -84,31 +50,65 @@ mod_cloropleth_server <- function(input, output, session, rv, global) {
         ) %>%
         # leaflet::addTiles() %>%
         leaflet::setView(lat = 20, lng = 10, zoom = 2.2) %>%
-        leaflet::addPolygons(
-          layerId = ~country_code,
-          fill = "white",
-          stroke = TRUE,
-          smoothFactor = 1,
-          fillOpacity = 1,
-          color = "#1F2430",
-          weight = 1,
-          label = mytext,
-          labelOptions = leaflet::labelOptions(
-            style = list("font-weight" = "normal", padding = "3px 8px", "background-color" = "#FAFAFA"),
-            textsize = "13px",
-            direction = "auto"
-          ),
-          fillColor = ~ my_palette(log(rv$map_data[[column]])),
-          highlightOptions = leaflet::highlightOptions(
-            color = "#FAFAFA", opacity = 1, weight = 2, fillOpacity = 1,
-            bringToFront = TRUE, sendToBack = TRUE)
-        ) 
-        # leaflet::addLegend(
-        #   pal = my_palette,
-        #   values = ~ rv$selected_variable,
-        #   opacity = 0.9,
-        #   title = title,
-        #   position = "bottomleft"
-        # )
+        add_polygons(rv) %>%
+        leaflet::addControl(html = legend_hint(), position = "topright")
+      # leaflet::addLayersControl(baseGroups = c("confirmed_cases", "confirmed_deaths", "confirmed_recovered"))
+      # leaflet::addLegend(
+      #   pal = my_palette,
+      #   values = ~ rv$selected_variable,
+      #   opacity = 0.9,
+      #   title = title,
+      #   position = "bottomleft"
+      # )
     })
+}
+
+
+add_polygons <- function(map, rv) {
+  get_quantiles <- function(metric, n = 666) {
+    qs <- seq(from = 0, to = 1, by = 1 / n)
+    bins <- unique(floor(quantile(log(metric), qs) %>% as.vector()))
+    c(bins, Inf)
+  }
+  
+  create_gradient <- function(col1, col2) {
+    fn_cols <- grDevices::colorRamp(c(col1, col2), space = "Lab", interpolate = "spline")
+    cols <- fn_cols(seq(0, 1, length.out = 10)) / 255
+    grDevices::rgb(cols[, 1], cols[, 2], cols[, 3], alpha = 1)
+  }
+
+  # Prepare text for the tooltip
+  mytext <- paste0(
+    "<b> Country: </b> ", rv$map_data$NAME, "<br/>",
+    "<hr>",
+    "<b> Infected: </b> ", prettyNum(rv$map_data[["confirmed_cases"]], big.mark = ","), "<br/>",
+    "<b> Deaths: </b> ", prettyNum(rv$map_data[["confirmed_deaths"]], big.mark = ","), "<br/>",
+    "<b> Recovered: </b> ", prettyNum(rv$map_data[["confirmed_recovered"]], big.mark = ","), "<br/>"
+  ) %>%
+    lapply(htmltools::HTML)
+
+  colours <- create_gradient(col1 = global$colours$grey, col2 = global$colours$orange)
+  my_palette <- leaflet::colorBin(colours, rv$map_data[["confirmed_cases"]], na.color = "white", bins = get_quantiles(rv$map_data[["confirmed_cases"]]))
+
+  map %>%
+    leaflet::addPolygons(
+      layerId = ~country_code,
+      fill = "white",
+      stroke = TRUE,
+      smoothFactor = 1,
+      fillOpacity = 1,
+      color = "#1F2430",
+      weight = 1,
+      label = mytext,
+      labelOptions = leaflet::labelOptions(
+        style = list("font-weight" = "normal", padding = "3px 8px", "background-color" = "#FAFAFA"),
+        textsize = "13px",
+        direction = "auto"
+      ),
+      fillColor = ~ my_palette(log(rv$map_data[["confirmed_cases"]])),
+      highlightOptions = leaflet::highlightOptions(
+        color = "#FAFAFA", opacity = 1, weight = 2, fillOpacity = 1,
+        bringToFront = TRUE, sendToBack = TRUE
+      )
+    )
 }
